bb30900f3f2ff517cde9bad31b63311dc3adb0f6
MohammadDaoud02 <152713131+MohammadDaoud02@users.noreply.github.com>
2023-12-02 19:00:55 +0300

    Add files via upload
    mohammad daoud 
    id: 202220400

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visual_programing
+{
+    internal class customer
+    {
+        
+        private name(string) { }
+        private address() { }
+
+        }
+    class order
+    {
+        private DateTime date;
+        private string status;
+        public calcsubtotal()
+        {
+            
+        }
+        public calctax()
+        {
+
+        }
+            public calctotal()
+        {
+
+        }
+            public calctotalweight()
+        {
+            
+        }
+        class Payment
+        {
+            private amount(float)
+
+                 class cash()
+            {
+                private cashtendered(float)
+             }
+         class check
+            {
+                private name (string)
+                    private bankid (string)
+                    public authorized()
+            }
+            class credit
+            {
+                private name (string)
+                    private type (string)
+                    private expdate (DateTime)
+                    public authorized()
+
+            }
+        }
+            
+
+        }
+    }
+    class orderdetail
+    {
+        private string quality() { }
+        private string taxStatus() { }
+        public  cacsubtotal()
+        {
+            
+        }
+        public Calcweight()
+        {
+
+        }
+        public calctax()
+        {
+
+        }
+    }
+    class item
+    {
+        private shippingweight() { }
+
+        private description(string) { }
+
+        public getpriceforQuantity()
+        {
+            
+        }
+        public getTax()
+        {
+
+        }
+        public inStock()
+        {
+
+        }
+    }
+}
